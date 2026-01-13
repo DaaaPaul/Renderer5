f35b7dc0ac1bffcbc5864d4ee0422056d4820083
DaaaPaul <paulpeng2008@gmail.com>
2026-01-12 19:34:37 -0500

    Some nice base for Memory class

--- source/vulkan/Memory.cpp
+++ source/vulkan/Memory.cpp
@@ -1,5 +1,66 @@
 #include "Memory.h"
+#include <iostream>
 
 namespace Vulkan {
+	Memory::Memory(Swapchain&& salvageSwapchain) {
+		std::cout << "---CREATING MEMORY...---";
+	}
 
+	Memory::Memory(Memory&& salvageMemory) {
+	
+		std::cout << "---MOVED MEMORY---";
+	}
+
+	Memory::~Memory() {
+		if(!isSalvagedRemains) {
+			std::cout << "---CLEANING MEMORY...---";
+		}
+	}
+
+	void createVerticiesBuffer() {
+	
+	}
+
+	void createIndicesBuffer() {
+	
+	}
+
+	void Memory::allocateMemory(VkDeviceMemory& memory, VkDeviceSize byteSize, uint32_t memoryTypeIndex) {
+		VkMemoryAllocateInfo memoryAllocateInfo = {
+			.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO,
+			.pNext = nullptr,
+			.allocationSize = byteSize,
+			.memoryTypeIndex = memoryTypeIndex
+		};
+
+		if(vkAllocateMemory(swapchain.queues.backend.device, &memoryAllocateInfo, nullptr, &memory) == VK_SUCCESS) {
+			std::cout << "Allocated device memory with " << memoryAllocateInfo.allocationSize << " size and " << memoryAllocateInfo.memoryTypeIndex << " memory type\n";
+		} else {
+			throw std::runtime_error("Memory creation failure");
+		}
+	}
+
+	void Memory::createBuffer(VkBuffer& buffer, VkDeviceSize byteSize, VkBufferUsageFlags usage) {
+		VkBufferCreateInfo bufferInfo = {
+			.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
+			.pNext = nullptr,
+			.flags = 0,
+			.size = byteSize,
+			.usage = usage,
+			.sharingMode = VK_SHARING_MODE_EXCLUSIVE,
+			.queueFamilyIndexCount = 1,
+			.pQueueFamilyIndices = &graphicsQueueFamilyIndex
+		};
+
+		if (vkCreateBuffer(swapchain.queues.backend.device, &bufferInfo, nullptr, &buffer) == VK_SUCCESS) {
+			std::cout << "Created buffer with " << bufferInfo.size << " size\n";
+		} else {
+			throw std::runtime_error("Memory creation failure");
+		}
+	}
+
+
+	void Memory::copyBuffer() {
+
+	}
 }
--- source/vulkan/Memory.h
+++ source/vulkan/Memory.h
@@ -1,7 +1,52 @@
 #pragma once
 
+#include "Swapchain.h"
+#include <vulkan/vulkan.h>
+#include <vector>
+
 namespace Vulkan {
+	class Pipeline;
+	class Sync;
+	class Commands;
+	class Engine;
+
 	class Memory {
+		friend class Pipeline;
+		friend class Sync;
+		friend class Commands;
+		friend class Engine;
+
+	private:
+		bool isSalvagedRemains;
+
+		uint32_t graphicsQueueFamilyIndex;
+
+		Swapchain swapchain;
+		VkBuffer stagingBuffer;
+		VkDeviceMemory stagingBufferM;
+		VkBuffer verticiesBuffer;
+		VkDeviceMemory verticiesBufferM;
+		VkBuffer indicesBuffer;
+		VkDeviceMemory indicesBufferM;
+		VkDescriptorSetLayout descriptorSetLayout;
+		VkDescriptorPool descriptorPool;
+		VkDescriptorSet descriptorSet;
+		std::vector<VkBuffer> uniformBuffers;
+		std::vector<VkDeviceMemory> uniformBuffersM;
+		std::vector<void*> uniformBuffersA;
+
+		void createVerticiesBuffer();
+		void createIndicesBuffer();
+
+		void allocateMemory(VkDeviceMemory& memory, VkDeviceSize byteSize, uint32_t memoryTypeIndex);
+		void createBuffer(VkBuffer& buffer, VkDeviceSize byteSize, VkBufferUsageFlags usage);
+		void copyBuffer();
+	public:
+		Memory(Swapchain&& salvageSwapchain);
+		Memory(Memory&& salvageMemory);
+		~Memory();
+
+		Memory(Memory const&) = delete;
+		Memory& operator=(Memory const&) = delete;
 	};
 }
-
