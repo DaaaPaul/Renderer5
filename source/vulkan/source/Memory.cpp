05755e8ba1362caef18dd10ab370e8db862f548c
DaaaPaul <paulpeng2008@gmail.com>
2026-01-21 21:39:15 -0500

    Update sampler mipmap parameters

--- source/vulkan/source/Memory.cpp
+++ source/vulkan/source/Memory.cpp
@@ -614,7 +614,7 @@ namespace Vulkan {
 			.compareEnable = false,
 			.compareOp = VK_COMPARE_OP_ALWAYS,
 			.minLod = 0.0f,
-			.maxLod = 0.0f,
+			.maxLod = VK_LOD_CLAMP_NONE,
 			.borderColor = VK_BORDER_COLOR_FLOAT_OPAQUE_BLACK,
 			.unnormalizedCoordinates = false,
 		};
